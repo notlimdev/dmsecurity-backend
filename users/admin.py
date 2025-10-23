f236d18fafa07995e1d26dbe02f85166897cea30
notlim_dev <elmotigre6@gmail.com>
2025-10-22 22:44:01 -0500

    agregamos el codigo ena dmin de users.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        BaseUserAdmin.fieldsets
+        + (("Información adicional", {"fields": ("role", "phone", "organization")}),)
+        if BaseUserAdmin.fieldsets
+        else (("Información adicional", {"fields": ("role", "phone", "organization")}),)
+    )
+
+    list_display = ("username", "email", "role", "is_active", "is_staff")
+    list_filter = ("role", "is_staff", "is_superuser", "is_active")
