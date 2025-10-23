56b7dad844a17e82555f20554215490feecda6dd
notlim_dev <elmotigre6@gmail.com>
2025-10-23 13:17:28 -0500

    agregamos la url endpoint configuracion general.

--- dmsecurity/urls.py
+++ dmsecurity/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
     path("api/risk/", include("risk.urls")),
+    path("api/security/", include("security.urls")),
 ]
