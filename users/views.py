ddad9d4cab3c2aac14501f33a4050625bae69a1d
notlim_dev <elmotigre6@gmail.com>
2025-10-22 22:45:19 -0500

    añadimos el codigo de vista.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API para gestionar usuarios (listar, crear, editar, eliminar).
+    """
+
+    queryset = User.objects.all().order_by("id")
+    serializer_class = UserSerializer
+    permission_classes = [
+        permissions.AllowAny
+    ]  # luego lo cambiamos a permisos más seguros
