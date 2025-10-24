1f038ca59c0c197fefc0be5571906697cf253d0f
notlim_dev <elmotigre6@gmail.com>
2025-10-24 16:47:50 -0500

    generamos los endpoint de jwt autentication

--- dmsecurity/urls.py
+++ dmsecurity/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
@@ -25,4 +27,6 @@ urlpatterns = [
     path("api/security/", include("security.urls")),
     path("api/mitigation/", include("mitigation.urls")),
     path("api/incident/", include("incident.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
