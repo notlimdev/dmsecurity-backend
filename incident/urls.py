60201f94242441b303dea1e50d6dd52991300294
notlim_dev <elmotigre6@gmail.com>
2025-10-24 15:32:16 -0500

    agregamos el endpoint para incidentes

---
+++ incident/urls.py
@@ -0,0 +1,7 @@
+from rest_framework.routers import DefaultRouter
+from .views import IncidentViewSet
+
+router = DefaultRouter()
+router.register(r"incidents", IncidentViewSet, basename="incident")
+
+urlpatterns = router.urls
