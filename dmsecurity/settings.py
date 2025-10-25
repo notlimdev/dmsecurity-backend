621d9aedb3af05e458146514ffa489e21ca9f826
notlim_dev <elmotigre6@gmail.com>
2025-10-25 12:32:27 -0500

    configuracion de api_key en las cofniguraciones generales del proyecto.

--- dmsecurity/settings.py
+++ dmsecurity/settings.py
@@ -165,3 +165,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+OPENAI_API_KEY = env("OPENAI_API_KEY")
